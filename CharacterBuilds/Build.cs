f4863306714e13d74d6d5d983a3668b658e67917
Justin <jwhenry123@gmail.com>
2020-11-11 22:20:38 -0600

    WIP working on builds

---
+++ CharacterBuilds/Build.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterBuilds
+{
+    public class Build
+    {
+        public int buildIndex { get; set; }
+        public int playerEntityId { get; set; }
+        public Dictionary<int, BuildHotBar> hotBars { get; set; }
+        public Dictionary<string, BuildGearItem> gear { get; set; }
+
+        public Build()
+        {
+            hotBars = new Dictionary<int, BuildHotBar>();
+            hotBars.Add(1, new BuildHotBar(1));
+            gear = new Dictionary<string, BuildGearItem>();
+        }
+    }
+}
---
+++ CharacterBuilds/BuildHotBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CharacterBuilds
+{
+    public class BuildHotBarHotKey
+    {
+        public int hotkeyIndex;
+        public string hotkeyId;
+        public HotkeyType type;
+    }
+    public class BuildHotBar
+    {
+        public int hotBarIndex { get; set; }
+        public List<BuildHotBarHotKey> hotKeys { get; set; }
+
+        public BuildHotBar(int index = 1)
+        {
+            hotBarIndex = index;
+            hotKeys = new List<BuildHotBarHotKey>();
+        }
+    }
+}
